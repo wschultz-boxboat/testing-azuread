fe19fb1d749cf5e644a218789ca4d5ea33c5bc8d
Will Schultz <will.schultz@ibm.com>
2022-11-21 08:58:25 -0600

    forcing the provider to not use my az cli creds

--- provider.tf
+++ provider.tf
@@ -11,4 +11,5 @@ provider "azuread" {
 #  tenant_id = "00000000-0000-0000-0000-000000000000" # ARM_TENANT_ID env var
 #  client_id = "" # ARM_CLIENT_ID env var
 #  client_secret = "" # ARM_CLIENT_SECRET env var
+    use_cli = false
 }
