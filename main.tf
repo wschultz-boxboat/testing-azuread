b2d194e93d32de63d533ea8ca00f44af3b82d07b
Will Schultz <will.schultz@ibm.com>
2022-11-21 10:04:12 -0600

    confirmed update of description

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azuread_group" "this" {
     assignable_to_role = false
-    description = "testing group for identitifying roles needed for service principal"
+    description = "testing group for identitifying roles needed for service principal and now making a change"
     display_name = "mars-terraform-testing"
     prevent_duplicate_names = true
     security_enabled = true
