3e3902fc214053bff053147b179726e2d744382a
Will Schultz <will.schultz@ibm.com>
2022-11-21 10:07:39 -0600

    confirmed ability to delete group

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "azuread_group" "this" {
-    assignable_to_role = false
-    description = "testing group for identitifying roles needed for service principal and now making a change"
-    display_name = "mars-terraform-testing"
-    prevent_duplicate_names = true
-    security_enabled = true
-}
+# resource "azuread_group" "this" {
+#     assignable_to_role = false
+#     description = "testing group for identitifying roles needed for service principal and now making a change again"
+#     display_name = "mars-terraform-testing"
+#     prevent_duplicate_names = true
+#     security_enabled = true
+# }
